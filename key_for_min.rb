b35cc7d60d40f6ab74ae53bde73341071a5c1532
Marc Mills <mdmills1990@gmail.com>
2018-08-24 17:29:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,10 +6,12 @@ def key_for_min_value(name_hash)
       return nil
     end
     comparison_value = 1000
+    key_for_comparison_value = nil
   key_for_min_value.each do |name, value|
       if value < comparison_value
         comparison_value = value
-        name
+        key_for_comparison_value = name
       end
     end
+    returns comparison_value
   end
